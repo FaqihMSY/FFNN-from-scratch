c4500b265d6a647744ff3f0e42667ea6509d0520
mlqmn <13523044@std.stei.itb.ac.id>
2026-03-15 17:31:31 +0700

    refactor: ffnn class for performance
    semoga beneran lebih cepat

--- src/ffnn.py
+++ src/ffnn.py
@@ -21,7 +21,7 @@
 import numpy as np
 from unit import Unit
 
-class ForwardPropagation :
+class FFNN :
     # DESKRIPSI LOKAL
     # Forward Propagation implementation in FFNN (Feed Forward Neural Network).
 
@@ -29,27 +29,53 @@ class ForwardPropagation :
     # count_net_per_layer , activate_layer , forward_propagation : Function
     
     # ALGORITMA
-    def count_net_per_layer(self : "ForwardPropagation" , last_layer : np.ndarray | list[Unit] , current_layer : list[Unit]) -> list[Unit] :
+
+    def __init__(self: "FFNN",
+                 dimension: list[int],
+                 activation_functions: list):
+        """
+        initializes a feed forward neural network.
+        dimension: numbers of unit in each layer, starting from the input layer
+                    up to the output layer.
+        activation_functions: activation function to use for each layer
+        """
+        self.weights = [np.random.rand(dimension[i+1], dimension[i]+1) for i in range(0, len(dimension)-1)]
+        self.net = []
+        self.out = []
+        self.activation_functions = activation_functions
+
+    def count_net_per_layer(self, last_layer : np.ndarray | int) :
         # DESKRIPSI LOKAL
         # Count net value for each unit (neuron) in current layer.
 
         # KAMUS LOKAL
-        # last_layer : any (np.ndarray | list[Unit])
+        # last_layer : np.ndarray | int
         # current_layer : list[Unit]
         # last : np.ndarray
         # len : Function
         # i : int (index)
 
         # ALGORITMA
-        if (isinstance(last_layer[0] , Unit)) :
-            last = np.array([unit.out for unit in last_layer])
-        else :
+        if (isinstance(last_layer , np.ndarray)) :
             last = last_layer
-        for (i) in range (len(current_layer)) :
-            current_layer[i].net = current_layer[i].predict([last])[0]
-        return current_layer
+            current_layer = 0
+        else:
+            last = self.out[last_layer]
+            current_layer = last_layer + 1
+        result = np.hstack(
+            [np.ones( (len(last), 1) ),
+            last]
+        )
+        result = result @ self.weights[current_layer].T
+        if len(self.net) >= current_layer + 1:
+            self.net[current_layer] = result
+        else:
+            assert(len(self.net) == current_layer)
+            self.net.append(result)
+        return self
 
-    def activate_layer(self : "ForwardPropagation" , layer : list[Unit] , activation_function : any) -> list[Unit] :
+    def activate_layer(self,
+                       layer : int) :
         # DESKRIPSI LOKAL
         # Activate current layer using given activation function.
 
@@ -61,13 +87,15 @@ class ForwardPropagation :
         # i : int (index)
 
         # ALGORITMA
-        net = np.array([unit.net for unit in layer])
-        res = activation_function.f(net)
-        for i in range (len(layer)) :
-            layer[i].out = res[i]
-        return layer
+        result = self.activation_functions[layer].f(self.net[layer])
+        if len(self.out) >= layer + 1:
+            self.out[layer] = result
+        else:
+            assert(len(self.out) == layer)
+            self.out.append(result)
+        return self
     
-    def forward_propagation(self : "ForwardPropagation" , input_layer : np.ndarray , hidden_layers : list[list[Unit]] , output_layer : list[Unit] , activation_function_hidden_layers : list[any] , activation_function_output_layer : any) -> list[np.ndarray , list[list[Unit]] , list[Unit]] :
+    def predict(self, input_layer : np.ndarray) :
         # DESKRIPSI LOKAL
         # Forward Propagation process in FFNN (Feed Forward Neural Network).
 
@@ -80,8 +108,11 @@ class ForwardPropagation :
         # i : int (index)
 
         # ALGORITMA
-        hidden_layers[0] = self.activate_layer(self.count_net_per_layer(input_layer , hidden_layers[0]) , activation_function_hidden_layers[0])
-        for i in range (1 , len(hidden_layers)) :
-            hidden_layers[i] = self.activate_layer(self.count_net_per_layer(hidden_layers[i - 1] , hidden_layers[i]) , activation_function_hidden_layers[i])
-        output_layer = self.activate_layer(self.count_net_per_layer(hidden_layers[len(hidden_layers) - 1] , output_layer) , activation_function_output_layer)
-        return [input_layer , hidden_layers , output_layer]
+        self.count_net_per_layer(input_layer).activate_layer(0)
+        
+        for i in range(1, len(self.weights)):
+            self.count_net_per_layer(i-1).activate_layer(i)
+        return self
+
+    def result(self):
+        return self.out[-1]
