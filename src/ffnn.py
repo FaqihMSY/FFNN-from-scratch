fix: delta calculation

src/ffnn.py
@@ -153,12 +153,21 @@ class FFNN :
         return self.out[-1]
     
     def compute_delta_output_layer(self, t):
+        if ((self.loss_function == CCE or self.loss_function == BCE) 
+                and self.activate_layer[-1] == Softmax):
+            self.delta.insert(0, self.out[-1][0] - t)
+            return
+
         o = self.out[-1][0]
         # print(f'{t=}')
         # print(f'{o=}')
         dL_do = self.loss_function.dL_do(t, o)
         # print(f'{dL_do=}')
-        dL_dnet = self.activation_functions[-1].df_dx(o) * dL_do
+        if self.activate_layer[-1] == Softmax:
+            J = Softmax.df_dx(o=o)
+            dL_dnet = J.T @ dL_do
+        else:
+            dL_dnet = self.activation_functions[-1].df_dx(o=o) * dL_do
         # print(f'{dL_dnet=}')
         self.delta.insert(0, dL_dnet)
 
@@ -175,7 +184,7 @@ class FFNN :
 
         dnet = w_next.T @ delta_next
         # print(f'{dnet=}')
-        do_dnet = self.activation_functions[layer-1].df_dx(o)  
+        do_dnet = self.activation_functions[layer-1].df_dx(o=o)  
         # print(f'{do_dnet=}')
         delta = do_dnet[:, np.newaxis] * dnet
         # print(f'{delta=}')

src/functions.py
@@ -23,14 +23,24 @@ import numpy as np
 class Linear :
     def f(x : np.ndarray) -> np.ndarray :
         return x
-    def df_dx(x : np.ndarray) -> np.ndarray :
-        return np.ones_like(x)
+    def df_dx(x : np.ndarray = None, o : np.ndarray = None) -> np.ndarray :
+        if x is None and o is None:
+            raise Exception("No value provided to df_dx")
+        elif o is None:
+            return np.ones_like(x)
+        else:
+            return np.ones_like(o)
 
 class ReLU :
     def f(x : np.ndarray) -> np.ndarray :
         return np.where(x > 0 , x , 0)
-    def df_dx(x : np.ndarray) -> np.ndarray :
-        return np.where(x > 0 , 1 , 0)
+    def df_dx(x : np.ndarray = None, o : np.ndarray = None) -> np.ndarray :
+        if x is None and o is None:
+            raise Exception("No value provided to df_dx")
+        elif o is None:
+            return np.where(x > 0 , 1 , 0)
+        else:
+            return np.where(o > 0 , 1 , 0)
 
 class Sigmoid:
     def _positive_sigmoid(x : np.ndarray) -> np.ndarray :
@@ -52,26 +62,40 @@ class Sigmoid:
         result[negative] = Sigmoid._negative_sigmoid(x[negative])
         return result
 
-    def df_dx(x : np.ndarray) -> np.ndarray :
-        sigm = Sigmoid.f(x)
-        return sigm * (1 - sigm)
+    def df_dx(x : np.ndarray = None, o : np.ndarray = None) -> np.ndarray :
+        if x is None and o is None:
+            raise Exception("No value provided to df_dx")
+        elif o is None:
+            sigm = Sigmoid.f(x)
+            return sigm * (1 - sigm)
+        else:
+            return o * (1 - o)
 
 class Tanh :
     def f(x : np.ndarray) -> np.ndarray :
         return np.tanh(x)
-    def df_dx(x : np.ndarray) -> np.ndarray :
-        t = np.tanh(x)
-        return 1 - t**2
+    def df_dx(x : np.ndarray = None, o : np.ndarray = None) -> np.ndarray :
+        if x is None and o is None:
+            raise Exception("No value provided to df_dx")
+        elif o is None:
+            t = np.tanh(x)
+            return 1 - np.power(t, 2)
+        else:
+            return 1 - np.power(o, 2)
 
 class Softmax:
-    def f(x : np.ndarray) -> np.ndarray :
-        e = np.exp(x)
-        sum = np.sum(e)
-        return e / sum
-    def df_dx(x : np.ndarray) -> np.ndarray :
-        sm = Softmax.f(x)
-        partial = sm * (np.eye(len(x)) - np.vstack(sm))
-        return partial
+    def f(x: np.ndarray) -> np.ndarray:
+        e = np.exp(x - np.max(x))
+        return e / np.sum(e)
+
+    def df_dx(x: np.ndarray = None, o: np.ndarray = None) -> np.ndarray:
+        if x is None and o is None:
+            raise Exception("No value provided to df_dx")
+        elif o is None:
+            sm = Softmax.f(x)
+            return sm * (np.eye(len(sm)) - np.vstack(sm))
+        else:
+            return o * (np.eye(len(o)) - np.vstack(o))
 
 class MSE:
     def L(t : np.ndarray , o : np.ndarray) -> np.ndarray :